2617b7c8844cb03a8721350ab7db5c631d8f1421
HUDEC <stephane.hudec@atos.net>
2018-07-30 15:26:48 +0200

    Partie 2: Tri par ordre alphabétique

--- src/main/java/com/ecommerce/microcommerce/dao/ProductDao.java
+++ src/main/java/com/ecommerce/microcommerce/dao/ProductDao.java
@@ -1,22 +1,26 @@
 package com.ecommerce.microcommerce.dao;
 
-import com.ecommerce.microcommerce.model.Product;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.ecommerce.microcommerce.model.Product;
 
 @Repository
 public interface ProductDao extends JpaRepository<Product, Integer> {
 
-    Product findById(int id);
+	Product findById(int id);
 
-    List<Product> findByPrixGreaterThan(int prixLimit);
+	List<Product> findByPrixGreaterThan(int prixLimit);
 
-    List<Product> findByNomLike(String recherche);
+	List<Product> findByNomLike(String recherche);
 
-    @Query("SELECT id, nom, prix FROM Product p WHERE p.prix > :prixLimit")
-    List<Product>  chercherUnProduitCher(@Param("prixLimit") int prix);
+	@Query("SELECT id, nom, prix FROM Product p WHERE p.prix > :prixLimit")
+	List<Product> chercherUnProduitCher(@Param("prixLimit") int prix);
+
+	Iterable<Product> findAllByOrderByNom();
+	
 }
--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -1,5 +1,27 @@
 package com.ecommerce.microcommerce.web.controller;
 
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.ecommerce.microcommerce.dao.ProductDao;
 import com.ecommerce.microcommerce.model.Product;
 import com.ecommerce.microcommerce.vo.ProductMargeValueObject;
@@ -7,18 +29,9 @@ import com.ecommerce.microcommerce.web.exceptions.ProduitIntrouvableException;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.json.MappingJacksonValue;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import javax.validation.Valid;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 
 @Api(description = "API pour es opérations CRUD sur les produits.")
 
@@ -100,6 +113,16 @@ public class ProductController {
 			productMargeVOList.add(pmvo);
 		}
 		return productMargeVOList;
+	}
+
+//	@RequestMapping(value = "/Produits", params = "sort")
+//	public Iterable<Product> trierProduitsParOrdreAlphabetique(@RequestParam("sort") String sort) {
+//		return productDao.findAll(new Sort(sort));
+//	}
+	
+	@RequestMapping(value = "/ProduitsOrderByNom")
+	public Iterable<Product> trierProduitsParOrdreAlphabetique() {
+		return productDao.findAllByOrderByNom();
 	}
 
 	// Pour les tests
